8879ca6e85db01484e4f4e823abb2dabf5577bf9
Pressly <130832755+itspressly@users.noreply.github.com>
2024-11-27 21:58:24 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+
 // Supabase API Configuration
 const SUPABASE_URL = "https://ldjyrvfxoxsjuxxvekpw.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkanlydmZ4b3hzanV4eHZla3B3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3MjYyNDQsImV4cCI6MjA0ODMwMjI0NH0.scu-KOTK00cTMeSk0NSaQ_o6wbVdJt331iQ0l8Ps618";
